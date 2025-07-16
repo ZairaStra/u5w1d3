a8f5ab92273daf681de1e6431a73a7306d394031
ZairaStra <straticozaira@gmail.com>
2025-07-16 17:10:33 +0200

    test multiparametrico

--- src/test/java/zairastra/u5w1d3/U5w1d3ApplicationTests.java
+++ src/test/java/zairastra/u5w1d3/U5w1d3ApplicationTests.java
@@ -1,6 +1,7 @@
 package zairastra.u5w1d3;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,14 +9,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class U5w1d3ApplicationTests {
 
-    @Test
-    void totalCoverCharge() {
-        System.out.println("TEST 1: verifico che il calcolo del costo totale dei coperti sia corretto");
-        double coverCharge = 2.5;
-        double fakeCoverNumb = 3;
+//    @Test
+//    void totalCoverCharge() {
+//        System.out.println("TEST 1: verifico che il calcolo del costo totale dei coperti sia corretto");
+//        double coverCharge = 2.5;
+//        double fakeCoverNumb = 3;
+//
+//        double result = coverCharge * fakeCoverNumb;
+//        assertEquals(7.5, result);
+//    }
 
+    //lo rendo multiparametrico: costo coperto, numero persone al tavolo, totale atteso
+    //funziona sia per numeri giusti che sbagliati
+
+    @ParameterizedTest
+    @CsvSource({
+            "2.5, 3, 7.5",
+            "2.5, 4, 10",
+            "2.5, 6, 15",
+            "2.5, 10, 25"
+    })
+    void totalCoverCharge(double coverCharge, int fakeCoverNumb, double expectedResult) {
         double result = coverCharge * fakeCoverNumb;
-        assertEquals(7.5, result);
+        assertEquals(expectedResult, result);
     }
 
 }
